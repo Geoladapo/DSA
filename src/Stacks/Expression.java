d89f21d76fea9f549b1c5ae46cf793e4852eaad0
oladapo <oladapoelijah49@gmail.com>
2024-10-05 20:49:11 +0100

    Expression matching with stack

--- src/Stacks/Expression.java
+++ src/Stacks/Expression.java
@@ -1,28 +1,43 @@
 package Stacks;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
+
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '{', '[');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', '{', ']');
+
     public boolean isBalanced(String input) {
 
         Stack<Character> stack = new Stack<>();
 
         for (char ch : input.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[' || ch == '<') {
+            if (isLeft(ch)) {
                 stack.push(ch);
             }
 
-            if (ch == ')' || ch == '}' || ch == ']' || ch == '>') {
+            if (isRight(ch)) {
                 if (stack.empty()) return false;
+
                 var top = stack.pop();
-                if ((
-                        ch == ')' && top != '(') ||
-                        (ch == '>' && top != '<') ||
-                        (ch == '{' && top != '{') ||
-                        (ch == '[' && top != '[')
-                ) return false;
+                if (bracketsMatch(top, ch)) return false;
             }
         }
         return stack.empty();
+    }
+
+    private boolean isLeft(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRight(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
