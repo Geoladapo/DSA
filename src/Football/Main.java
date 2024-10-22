d3bba362ae88d82b4acd314aae8ef3d22aa71599
oladapo <oladapoelijah49@gmail.com>
2024-10-22 15:42:17 +0100

    generic

--- src/Football/Main.java
+++ src/Football/Main.java
@@ -1,11 +1,29 @@
 package Football;
 
+interface Player {
+    String name();
+}
 
-record BaseBallPlayer(String name, String position){}
+record BaseBallPlayer(String name, String position) implements Player {}
+record FootballPlayer(String name, String position) implements Player {}
+record VolleyBallPlayer(String name, String position) implements Player {}
+
 public class Main {
     public static void main(String[] args) {
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Houston Astros");
+
+        var philly = new Affiliation("city", "Philadelphia, PA", "US");
+
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros1 = new BaseballTeam("Houston Astros");
+        scoreResult(phillies1,3, astros1, 5);
+
+
+        SportTeam phillies2 = new SportTeam("Philadelphia Phillies");
+        SportTeam astros2 = new SportTeam("Houston Astros");
+        scoreResult(phillies2,3, astros2, 5);
+
+        Team<BaseBallPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseBallPlayer, Affiliation> astros = new Team<>("Houston Astros");
         scoreResult(phillies,3, astros, 5);
 
         var harper = new BaseBallPlayer("B Harper", "Right Fielder");
@@ -13,10 +31,37 @@ public class Main {
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
         phillies.listTeamMembers();
+
+        SportTeam afc1 = new SportTeam("Adelaide Crows");
+        Team<FootballPlayer, Affiliation> afc = new Team<>("Adelaide Crows");
+        var tex = new FootballPlayer("Tex Walker", "Centre half forward");
+        afc.addTeamMember(tex);
+        var rory = new FootballPlayer("Rory Laird", "Midfielder");
+        afc.addTeamMember(rory);
+        afc.listTeamMembers();
+
+
+        Team<VolleyBallPlayer, Affiliation> adelaide = new Team<>("Adelaide Storm");
+        adelaide.addTeamMember(new VolleyBallPlayer("N Roberts", "Setter"));
+        adelaide.listTeamMembers();
     }
 
     public  static void scoreResult(BaseballTeam team1, int t1_score,
                                     BaseballTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public  static void scoreResult(SportTeam team1, int t1_score,
+                                    SportTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public  static void scoreResult(Team team1, int t1_score,
+                                    Team team2, int t2_score) {
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
---
+++ src/Football/Team.java
@@ -0,0 +1,69 @@
+package Football;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+record Affiliation(String name, String type, String countryCode){
+    @Override
+    public String toString() {
+        return name + " (" + type + " in " + countryCode + ")";
+    }
+}
+
+public class Team<T extends Player, S> {
+    private String teamName;
+    private List<T> teamMembers = new ArrayList<>();
+    private int totalWins = 0;
+    private int totalLosses = 0;
+    private int totalDraws = 0;
+    private S affiliation;
+
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public Team(String teamName, S affiliation) {
+        this.teamName = teamName;
+        this.affiliation = affiliation;
+    }
+
+    public void addTeamMember(T teamMember) {
+        if (!teamMembers.contains(teamMember)) {
+            teamMembers.add(teamMember);
+        }
+    }
+
+    public void listTeamMembers() {
+        System.out.print(teamName + " Roster");
+        System.out.println((affiliation == null ? "" : "AFFILIATION:" + affiliation));
+        for(T t : teamMembers){
+            System.out.println(t.name());
+        }
+    }
+
+    public int ranking() {
+        return (totalLosses * 2) + totalDraws + 1;
+    }
+
+    public String setScore(int ourScore, int theirScore) {
+        String message = "lost to";
+        if (ourScore > theirScore) {
+            totalWins++;
+            message = "beat";
+        } else if (ourScore == theirScore) {
+            totalDraws++;
+            message = "draw";
+        } else {
+            totalLosses++;
+        }
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return
+                teamName + " (Ranked " + ranking() + ")";
+    }
+}
