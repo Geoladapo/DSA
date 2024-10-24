f129fbc81ad80906106c2d97d4bbc4439e2f0065
oladapo <oladapoelijah49@gmail.com>
2024-10-24 17:51:04 +0100

    bubble sort

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Integer five = 5;
+        Integer[] others = {0, 5, 10, -50, 50};
+
+        for (Integer i : others) {
+            int val = five.compareTo(i);
+            System.out.println("%d %s %d: compareTo=%d%n" +
+                    (val == 0 ? "==" : (val < 0) ? "<" : ">"));
         }
     }
 }
